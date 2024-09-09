7485fcd96d30cc54c42746637f3151f59c8a8643
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-09 11:54:53 -0500

    feat: add multer, configure uploads and validators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller, FileTypeValidator, MaxFileSizeValidator,
+  ParseFilePipe, Post, UploadedFile, UseInterceptors
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import multer, { diskStorage } from 'multer';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor() { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (_, file, cb) => {
+        cb(null, file.originalname);
+      }
+    })
+  }))
+  uploadFile(@UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }),
+        new FileTypeValidator({ fileType: "image/*" })
+      ]
+    })
+  ) file: Express.Multer.File) {
+    console.log(file);
   }
 }
