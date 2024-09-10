1b3d1d093956a883e4e92c7c31a63dc1c04014e0
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-10 10:52:36 -0500

    feat: add fileservice to create in the db

---
+++ src/file/file.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, File } from '@prisma/client';
+
+@Injectable()
+export class FileService {
+    constructor(private prisma: PrismaService) { }
+
+    async createFile(data: Prisma.FileCreateInput): Promise<File> {
+        return this.prisma.file.create({ data })
+    }
+}
