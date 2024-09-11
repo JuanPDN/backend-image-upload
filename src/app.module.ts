ea4f31c545832738ac024f38c9bf3afedab04fa9
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-11 14:50:15 -0500

    chore: add serve static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static'
+
 import { AppController } from './app.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { FileService } from './file/file.service';
+import { join } from 'path';
+
 
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'uploads'),
+    serveRoot: '/uploads'
+  })],
   controllers: [AppController],
   providers: [PrismaService, FileService],
 })
