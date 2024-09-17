6539a857054205198cc5f2456b49c3a1bcd86d1f
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-16 20:32:38 -0500

    feat: creat cloudinary module and config cloudinary to upload images

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CloudinaryResponse } from './cloudinary-response';
+import { v2 as cloudinary } from 'cloudinary'
+import * as streamifier from 'streamifier'
+
+
+@Injectable()
+export class CloudinaryService {
+    uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+
+        return new Promise<CloudinaryResponse>((resolve, reject) => {
+
+            const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
+                if (error) return reject(error)
+                resolve(result)
+            })
+
+            streamifier.createReadStream(file.buffer).pipe(uploadStream)
+        })
+    }
+
+    getImageAttachment(publicID: string): string {
+        return cloudinary.url(publicID, {
+            flags: "attachment",
+        })
+    }
+}
